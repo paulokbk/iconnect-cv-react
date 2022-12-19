d1a5ff7fe2b1b071490be96b543ee51cb7d89805
Paulo Filho <paulokbkfilho@hotmail.com>
2022-12-19 00:28:31 -0300

    Iniciando tela de login

---
+++ src/views/Login/index.js
@@ -0,0 +1,19 @@
+import "./index.scss";
+
+export const Login = () => {
+  return (
+    <body>
+      <div className="max-width-content">
+        <div className="box-login">
+          <img src="./assets/logo.jpg" alt="Logo iConnect" className="logo" />
+          <h1 className="title">Controle de Vendas</h1>
+          <form className="display-flex flex-direction-column">
+            <input type="email" name="email" placeholder="Email"/>
+            <input type="password" name="password" placeholder="Senha"/>
+            <button type="submit">Acessar</button>
+          </form>
+        </div>
+      </div>
+    </body>
+  );
+};
